085c95fecb3e4d2cfb539afcb23450bb5a4317e7
SergioGI <63078859+SergioGI@users.noreply.github.com>
2022-05-20 08:32:30 -0500

    Add files via upload

---
+++ fragment2/Detalle.java
@@ -0,0 +1,45 @@
+package com.example.fragment2;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Detalle extends  Fragment {
+    int position = 0;
+    TextView tvTitle;
+    TextView tvDetails;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            if (getArguments() != null) {
+                position = getArguments().getInt("position", 0);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_detalle, parent, false);
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+        tvDetails = (TextView) view.findViewById(R.id.tvDetails);
+
+        tvTitle.setText(Pizza.pizzaMenu[position]);
+        tvDetails.setText(Pizza.pizzaDetails[position]);
+    }
+    public void updateView(int position){
+        tvTitle.setText(Pizza.pizzaMenu[position]);
+        tvDetails.setText(Pizza.pizzaDetails[position]);
+    }
+}
---
+++ fragment2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fragment2;
+
+import android.os.Bundle;
+import androidx.fragment.app.FragmentTransaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements lista.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            lista firstFragment = new lista();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.flContainer, firstFragment);
+            ft.commit();
+        }
+    }
+    public void onPizzaItemSelected(int position) {
+        Detalle secondFragment = new Detalle();
+
+        Bundle args = new Bundle();
+        args.putInt("position", position);
+        secondFragment.setArguments(args);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flContainer, secondFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+}
